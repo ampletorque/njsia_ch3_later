c86b8b8a611c4f88fecc1e72359925ba509396c8
Andrew <andrew@ampletorque.com>
2018-04-03 23:36:52 +0000

    routes

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const app = express();
+const articles = [{ title: 'Example' }];
 
-const port = process.env.PORT || 3000;
+app.set('port', process.env.PORT || 3000);
 
-app.get('/', (req, res) => {
-	  res.send('Hello World');
+app.get('/articles', (req, res, next) => {
+	res.send(articles);
 });
 
-app.listen(port, () => {
-	console.log(`Express web app available at localhost: ${port}`);
+app.post('/articles', (req, res, next) => {
+	res.send('OK');
 });
+
+app.get('/articles/:id', (req, res, next) => {
+	const id = req.params.id;
+	console.log('Fetching:', id);
+	res.send(articles[id]);
+});
+
+app.delete('/articles/:id', (req, res, next) => {
+	const id = req.params.id;
+	console.log('Deleting:', id);
+	delete articles[id];
+	res.send({ message: 'Deleted' });
+});
+
+app.listen(app.get('port'), () => {
+	console.log('App started on port', app.get('port'));
+});
+
+module.exports = app;
 
