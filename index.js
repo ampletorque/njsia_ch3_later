e7ab66f602a4661cb40a930a289a032e0f622657
Andrew <andrew@ampletorque.com>
2018-04-04 03:01:19 +0000

    db integrate

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const articles = [{ title: 'Example' }];
 const bodyParser = require('body-parser');
+const Article = require('./db').Article;
 
 app.set('port', process.env.PORT || 3000);
 
@@ -9,26 +9,26 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/articles', (req, res, next) => {
-	res.send(articles);
-});
-
-app.post('/articles', (req, res, next) => {
-	const article = { title: req.body.title };
-	articles.push(article);
-	res.send(article);
+	Article.all((err, articles) => {
+		if (err) return next(err);
+		res.send(articles);
+	});
 });
 
 app.get('/articles/:id', (req, res, next) => {
 	const id = req.params.id;
-	console.log('Fetching:', id);
-	res.send(articles[id]);
+	Aarticle.find(id, (err, article) => {
+		if (err) return next(err);
+         	res.send(articles[id]);
+	});
 });
 
 app.delete('/articles/:id', (req, res, next) => {
 	const id = req.params.id;
-	console.log('Deleting:', id);
-	delete articles[id];
-	res.send({ message: 'Deleted' });
+	Article.delete(id, (err) => {
+		if (err) return next(err);
+	        res.send({ message: 'Deleted' });
+	});
 });
 
 app.listen(app.get('port'), () => {
