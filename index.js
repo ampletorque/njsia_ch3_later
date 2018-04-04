6c68c57fee7f64b1996aa86c261a37b0a8289527
Andrew <andrew@ampletorque.com>
2018-04-04 03:25:14 +0000

    add readability

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const Article = require('./db').Article;
+const read = require('node-readability');
+
+const url = 'http://www.manning.com/cantelon2/';
+read(url, (err, result)=> {
+	// result has .title and .content
+});
 
 app.set('port', process.env.PORT || 3000);
 
@@ -30,6 +36,21 @@ app.delete('/articles/:id', (req, res, next) => {
 	        res.send({ message: 'Deleted' });
 	});
 });
+
+app.post('/articles', (req, res, next) => {
+	const url = req.body.url;
+    read(url {err, result) => {
+	if (err || !result) res.status(500).send('Error downloading article);
+		Article.create(
+			{ title: result.title, content: result.content },
+			(err, article) => {
+				if (err) return next(err);
+				res.send('OK');
+			}
+		);
+	});
+});
+
 
 app.listen(app.get('port'), () => {
 	console.log('App started on port', app.get('port'));
