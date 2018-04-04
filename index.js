e29cfffa188a3cc2697ae0eb187bfe392fb1e11b
Andrew <andrew@ampletorque.com>
2018-04-04 05:02:05 +0000

    add bootstrap and enable templates

--- index.js
+++ index.js
@@ -4,21 +4,28 @@ const bodyParser = require('body-parser');
 const Article = require('./db').Article;
 const read = require('node-readability');
 
-const url = 'http://www.manning.com/cantelon2/';
-read(url, (err, result)=> {
-	// result has .title and .content
-});
-
 app.set('port', process.env.PORT || 3000);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
+app.use(
+  '/css/bootstrap.css',
+  express.static('node_modules/bootstrap/dist/css/bootstrap.css')
+);
 
 app.get('/articles', (req, res, next) => {
 	Article.all((err, articles) => {
 		if (err) return next(err);
-		res.send(articles);
-	});
+
+    res.format({
+            html: () => {
+                    res.render('articles.ejs', { articles: articles });
+            },
+            json: () => {
+                    res.send(articles);
+            }       
+    });   
+  });
 });
 
 app.get('/articles/:id', (req, res, next) => {
